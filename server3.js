2a4a733e467293746007467af93ff839d23bbffc
kimeunyoung01 <rola70@nate.com>
2020-04-07 12:55:09 +0900

    상품추가

--- server3.js
+++ server3.js
@@ -14,6 +14,7 @@ var items=[
 ];
 
 app.use(express.static("public"));
+app.use(express.bodyParser());
 app.use(app.router);
 
 app.all("/data.html",function(request, respone){
@@ -38,6 +39,39 @@ app.all("/data.html",function(request, respone){
 app.all("/data.json",function(request, respone){
   respone.send(items);
 });
+
+app.get("/products/:id",function(request, response){
+  var id = Number(request.param("id"));
+  if(isNaN(id)){
+      response.send({error:"숫자를 입력하세요"})
+  }else if(items[id]){
+      response.send(items[id]);
+  }else{
+    response.send({error:"상품의 인덱스를 확인하세요"})
+  }
+
+  // response.send(items[id]);
+});
+
+app.post("/products", function(request, response){
+  try{
+      var name = request.param("name");
+      var price = request.param("price");
+      console.log("상품명:"+name);
+      console.log("상품각격:"+price);
+      var item = {name:name, price:price};
+      items.push(item);
+
+      response.send({message:"데이터를 추가했습니다",
+                      data:items});
+  }catch(ex){
+    console.log(ex);
+  }
+});
+
+
+
+
 
 app.all("/data.xml",function(request, respone){
     respone.type("text/xml");
