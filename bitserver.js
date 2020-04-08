8e007139d6b486f377b9ee9e45fac579d4482760
kimeunyoung01 <rola70@nate.com>
2020-04-08 17:37:02 +0900

    blog insert

---
+++ bitserver.js
@@ -0,0 +1,34 @@
+var http = require("http");
+var express = require("express");
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'bit';
+
+var app = express();
+app.use(express.static("public"));
+app.use(express.bodyParser());
+app.use(app.router);
+
+app.get("/blogInsert", function(request, respone){
+  var title = request.param("title");
+  var content = request.param("content");
+  var saved_at = new Date();
+  var doc = {title:title, content:content, saved_at:saved_at};
+
+    const client = new MongoClient(url);
+    client.connect(function(err, client) {
+    const db = client.db(dbName);
+    db.collection('articles').insertOne(doc, function(err, r) {
+      _id = r.insertedId;
+      client.close();
+      respone.send(_id);
+    });
+  });
+});
+
+
+http.createServer(app).listen(52273, function(){
+  console.log("서버가 가동되었습니다");
+});
